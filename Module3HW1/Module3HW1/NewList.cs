bd5bdd54575639bad1a35c2687b86fea2b1b5fb5
OMIS\Geiko_J <rewers212>
2021-07-28 11:40:54 +0300

    Add method

---
+++ Module3HW1/Module3HW1/NewList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW1
+{
+    public class NewList<T>
+    {
+        private const int DefaultCapacity = 4;
+        private T[] _items;
+        private int _size;
+        private static readonly T[] _emptyArray = new T[0];
+
+        public NewList()
+        {
+            _items = _emptyArray;
+        }
+
+        public int Capacity
+        {
+            get => _items.Length;
+            set
+            {
+                if (value != _items.Length)
+                {
+                    if (value > 0)
+                    {
+                        var newItems = new T[value];
+                        if (_size > 0)
+                        {
+                            Array.Copy(_items, newItems, _size);
+                        }
+
+                        _items = newItems;
+                    }
+                    else
+                    {
+                        _items = _emptyArray;
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in _items)
+            {
+                yield return item;
+            }
+        }
+
+        public void Add(T item)
+        {
+            var mass = _items;
+            var size = _size;
+            if (size < mass.Length)
+            {
+                _size = size + 1;
+                mass[size] = item;
+            }
+            else
+            {
+                AddWithResize(item);
+            }
+        }
+
+        public void AddWithResize(T item)
+        {
+            var size = _size;
+            EnsureCapacity(size + 1);
+            _size = size + 1;
+            _items[size] = item;
+        }
+
+        public void AddRange(T item)
+        {
+        }
+
+        public bool Remove(T item)
+        {
+            var index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < _size)
+            {
+                Array.Copy(_items, index + 1, _items, index, _size - index);
+            }
+
+            if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
+            {
+                _items[_size] = default;
+            }
+
+            _size--;
+        }
+
+        public void Sort()
+        {
+        }
+
+        public int IndexOf(T item)
+            => Array.IndexOf(_items, item, 0, _size);
+
+        private void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                var newCapacity = _items.Length == 0 ? DefaultCapacity : _items.Length * 2;
+                if (newCapacity < min)
+                {
+                    newCapacity = min;
+                }
+
+                Capacity = newCapacity;
+            }
+        }
+    }
+}
--- Module3HW1/Module3HW1/Program.cs
+++ Module3HW1/Module3HW1/Program.cs
@@ -6,7 +6,28 @@ namespace Module3HW1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var test = new NewList<int>();
+            test.Add(5);
+            test.Add(7);
+            test.Add(55);
+            test.Add(4);
+            test.Add(0);
+            test.Add(9);
+            test.Add(10);
+            test.Remove(7);
+            test.Remove(55);
+            test.Add(76);
+            test.Add(35);
+            test.Remove(4);
+            test.Add(99);
+            test.Add(0);
+            test.Add(22);
+            test.Remove(0);
+            test.RemoveAt(55);
+            foreach (var item in test)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
